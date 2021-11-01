1aea050f6c192065e8c25a249eeafde7fb2fdf42
Illidan <illidanrulez@gmail.com>
2021-11-01 12:13:31 +0100

    Bump version.

--- tool.py
+++ tool.py
@@ -3,7 +3,7 @@ import click
 import fpk
 from hacktools import common, psp
 
-version = "1.3.0"
+version = "1.3.1"
 isofile = "data/resonance.iso"
 isopatch = "data/resonance_patched.iso"
 patchfile = "data/patch.xdelta"
